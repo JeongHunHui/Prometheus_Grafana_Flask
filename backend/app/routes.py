a51e5a15bff6bd180eab44c01a51b06f8d92931b
JeongHunHui <deca3248@gmail.com>
2023-07-14 20:19:22 +0900

    feat: 숙소 검색 API 구현

---
+++ backend/app/routes.py
@@ -0,0 +1,34 @@
+from flask import request, jsonify
+import pymysql
+from app import app
+from app.db_manager import db_manager
+
+@app.route('/hotels', methods=['GET'])
+def find_hotels():
+    connection = db_manager.get_connection()
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    people_count = request.args.get('count')
+    region_id = request.args.get('region')
+    cursor_id = request.args.get('cursor')
+
+    sql = """
+            SELECT h.name       AS hotel_name,
+                   MIN(r.price) AS price,
+                   rating,
+                   address,
+                   detail_region_name,
+                   category_name
+            FROM hotels h
+                JOIN rooms r
+            		ON h.id = r.hotel_id
+            			AND r.max_people_count >= %s
+            WHERE h.region_id = %s
+                AND h.id > %s
+            GROUP BY h.id
+            ORDER BY h.id
+            LIMIT 20;
+        """
+    cursor.execute(sql, (people_count, region_id, cursor_id))
+
+    result = cursor.fetchall()
+    return jsonify(result)
