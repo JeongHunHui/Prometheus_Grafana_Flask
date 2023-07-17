cce90ff6c68c8a4ca49d0d8bc2f702ca5b06092d
JeongHunHui <deca3248@gmail.com>
2023-07-17 19:18:53 +0900

    feat: prometheus, grafana 컨테이너 세팅 및 Flask에서 메트릭을 수집하도록 세팅

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from .db_manager import DBManager, db_manager
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 
 db_manager = DBManager()
+
+metrics = PrometheusMetrics(app, register_defaults=True, default_latency_as_histogram=True, excluded_methods=["OPTIONS"])
+
+metrics.info('app_info', 'Application info', version='1.0.3')
 
 from app import routes
