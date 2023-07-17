1001744d2fe93907ebd1b976944c1fa3e60e832f
JeongHunHui <deca3248@gmail.com>
2023-07-18 03:21:16 +0900

    feat: prometheus_client 라이브러리로 메트릭 수집하도록 수정, api 요청 수, 응답시간, 에러 수 메트릭 수집

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,37 @@
-from flask import Flask
+from flask import Flask, request
 from .db_manager import DBManager, db_manager
-from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import Counter, Histogram, make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+import time
 
 app = Flask(__name__)
 
 db_manager = DBManager()
 
-metrics = PrometheusMetrics(app, register_defaults=True, default_latency_as_histogram=True, excluded_methods=["OPTIONS"])
+api_request_count = Counter('api_request_count', 'API Request Count',
+                            ['method', 'status', 'endpoint'])
+api_response_time = Histogram('api_response_time_seconds', 'API Response Time',
+                              ['method', 'endpoint'])
+api_error_count = Counter('api_error_count', 'API Error Count',
+                          ['method', 'status', 'endpoint'])
 
-metrics.info('app_info', 'Application info', version='1.0.3')
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
+@app.before_request
+def before_request():
+    request.start_time = time.time()
+
+@app.after_request
+def after_request(response):
+    request_latency = time.time() - request.start_time
+    status_code = response.status_code
+    api_request_count.labels(method=request.method, status=str(status_code), endpoint=request.endpoint).inc()
+    if status_code >= 400:
+        api_error_count.labels(method=request.method, status=str(status_code), endpoint=request.endpoint).inc()
+    elif status_code < 400:
+        api_response_time.labels(method=request.method, endpoint=request.endpoint).observe(request_latency)
+    return response
 
 from app import routes
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,15 +1,21 @@
 from flask import request, jsonify
 import pymysql
+import time
 from app import app
 from app.db_manager import db_manager
 
 @app.route('/hotels', methods=['GET'])
 def find_hotels():
+    start_time = time.time()
     connection = db_manager.get_connection()
     cursor = connection.cursor(pymysql.cursors.DictCursor)
     people_count = request.args.get('count')
     region_id = request.args.get('region')
+    category_id = request.args.get('category')
     cursor_id = request.args.get('cursor')
+
+    if not all(param is not None and param.isdigit() for param in [people_count, region_id, category_id, cursor_id]):
+        return "Invalid Parameters", 400
 
     sql = """
             SELECT h.name       AS hotel_name,
@@ -20,15 +26,18 @@ def find_hotels():
                    category_name
             FROM hotels h
                 JOIN rooms r
-            		ON h.id = r.hotel_id
-            			AND r.max_people_count >= %s
+                    ON h.id = r.hotel_id
+                        AND r.max_people_count >= %s
             WHERE h.region_id = %s
+                AND h.category_id = %s
                 AND h.id > %s
             GROUP BY h.id
             ORDER BY h.id
             LIMIT 20;
         """
-    cursor.execute(sql, (people_count, region_id, cursor_id))
+    cursor.execute(sql, (people_count, region_id, category_id, cursor_id))
 
     result = cursor.fetchall()
-    return jsonify(result)
+    status_code = 200 if result else 404
+
+    return jsonify(result), status_code
